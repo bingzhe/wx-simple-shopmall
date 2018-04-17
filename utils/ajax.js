38c42f0e4a991c2b3ed949d510f05d95e672c868
bingzhe <874177815@qq.com>
2018-04-17 17:38:30 +0800

    添加util, 封装wx.request

---
+++ utils/ajax.js
@@ -0,0 +1,226 @@
+import Util from './util'
+import { encrypt, decrypt } from './encrypt'
+
+const md5 = require("./md5.min.js")
+const RSA = require('./wxapp_rsa.js')
+// 域名
+const BASE_URL = 'http://shop.jzzwlcm.com/php/'
+
+/**
+ * 
+ * 终端标识token
+ */
+const createToken = function () {
+    let token = wx.getStorageSync('token')
+    if (!token) {
+        token = "T1" + Util.GetRandString(14)
+        wx.setStorageSync('token', token)
+    }
+    return token;
+}
+
+/**
+ * 前后台数据加密key
+ */
+const createKey = function () {
+
+    let token = createToken(),
+        key
+
+    // 取公钥
+    function get_publickey() {
+        return new Promise((resolve, reject) => {
+            let pubkey = ""
+
+            wx.request({
+                url: `${BASE_URL}rsa_info.php?${Util.GetRandString(3)}`,
+                dataType: 'json',
+                data: {
+                    "publickey": 1
+                },
+                success: resp => {
+                    if (resp.statusCode === 200 && resp.data.ret === 0) {
+                        pubkey = resp.data.data.publickey
+                        resolve(pubkey)
+                    } else {
+                        reject(resp)
+                    }
+                },
+                fail: resp => {
+                    reject(resp)
+                }
+            })
+        })
+    }
+
+    // 提交key到服务器
+    function submit_key(pubkey) {
+        return new Promise((resolve, reject) => {
+            key = Util.GetRandString(16) // 随机key
+            console.log("key:" + key)
+
+            //加密
+            // let rsa = new JSEncrypt();
+            // rsa.setPublicKey(pubkey);
+            // let key_enc = rsa.encrypt(key);
+
+            let encrypt_rsa = new RSA.RSAKey()
+            encrypt_rsa = RSA.KEYUTIL.getKey(pubkey)
+            let key_enc = encrypt_rsa.encrypt(key)
+
+            wx.request({
+                url: `${BASE_URL}rsa_info.php?${Util.GetRandString(3)}`,
+                dataType: "json",
+                data: {
+                    "save_key": 1,
+                    "is_plain": 1, // 是明文（配合后台DecSubmitData()用）
+                    "key_enc": key_enc,
+                    "token": token
+                },
+                success: resp => {
+                    if (resp.statusCode === 200 && resp.data.ret === 0) {
+                        wx.setStorageSync('key', key)
+                        resolve(key)
+                    } else {
+                        reject(resp)
+                    }
+                },
+                fail: resp => {
+                    reject(resp)
+                }
+            })
+        })
+    }
+
+    return get_publickey()
+        .then(submit_key)
+        .catch(resp => {
+            console.warn(resp)
+        })
+}
+
+// 只序列化对象
+const param = function (obj) {
+    if (!Util.isPlainObject(obj)) {
+        return
+    }
+
+    let s = [],
+        add = function (key, value) {
+            value = value == null ? "" : value;
+            s[s.length] = encodeURIComponent(key) + "=" + encodeURIComponent(value);
+        };
+
+
+    for (const key in obj) {
+        if (object.hasOwnProperty(key)) {
+            const element = obj[key]
+            add(key, element)
+        }
+    }
+
+    return s.join("&").replace(r20, "+");
+}
+/*
+* other : {
+*      //dataType: xxx,
+*      //mimeType: xxx,
+*      is_get_param: 1,            // 只返回要提交的参数（不执行提交动作）
+*      encmode: xxx                // 数据加密方式[""|encrypt1]
+*      ...
+* }
+*/
+const EncSubmit = function (url, data, resp_func/*, other --> {dataType:xxx, mimeType:xxx, ...}*/) {
+    if (!data instanceof Object) {
+        // param err
+        return;
+    }
+
+    let token = createToken()
+    let key = wx.getStorageSync('key')
+
+    if (!key) {
+        createKey()
+        key = wx.getStorageSync('key')
+    }
+
+    let data_type = null,
+        // mime_type = null,
+        is_get_param = null,
+        encmode = ""
+
+    if (arguments.length > 3 && Util.isPlainObject(arguments[3])) // 可变参数时
+    {
+        data_type = "json"  //arguments[3].dataType;
+        // mime_type = arguments[3].mimeType
+        is_get_param = arguments[3].is_get_param
+        encmode = arguments[3].encmode;
+    }
+
+    for (let item in data) {
+        if (typeof (data[item]) !== "string" && typeof (data[item]) !== "number" && typeof (data[item]) !== "boolean") {
+            data[item] = JSON.stringify(data[item]);
+        }
+    }
+
+    var datastr = param(data);
+
+    if ("encrypt1" == encmode) {
+        datastr = encrypt(key, datastr)
+    }
+
+    var param = {
+        token: token,
+        userid: wx.getStorageSync('userid'),
+        encmode: encmode,
+        data: datastr,
+        sign: md5(datastr + key)
+    };
+
+    // 只取参数
+    if (is_get_param) {
+        return param
+    }
+
+
+    wx.request({
+        url: `${BASE_URL}${url}?${(new Date()).getTime()}`,
+        method: "POST",
+        dataType: data_type || "json",
+        data: param,
+        success: function (resp) {
+            let resp = resp.data || {}
+
+            //未登录调回首页
+            // if (resp.ret === errcode.USER_NOLOGIN) {
+            //     router.push("/");
+            // }
+
+            //通讯key不存在，签名错误
+            // if (resp.ret === errcode.DATA_KEY_NOT_EXIST || resp.ret === errcode.SVC_ERR_DATA_SIGN) {
+            //     window.Store.DelGlobalData('key');
+            // }
+
+            if (typeof (resp_func) === 'function') {
+                if (0 === resp.ret && resp.crypt === "1" && resp.data !== "") {
+                    resp.data = JSON.parse(decrypt(key, resp.data))
+                    delete resp.crypt
+                }
+                return resp_func(resp)
+            }
+
+        },
+
+        error: function (resp) {
+            if (typeof (resp_func) === 'function') {
+                var ret = {
+                    ret: -1,
+                    data: "resp.responseText: " + resp.responseText
+                };
+                return resp_func(ret);
+            }
+        }
+    }) // end of wx.request({...
+}
+
+export default EncSubmit
---
+++ utils/util.js
@@ -0,0 +1,161 @@
+import http from './ajax'
+
+const _toString = Object.prototype.toString
+const hasOwnProperty = Object.prototype.hasOwnProperty
+
+const Util = {
+  isTrue: function (v) {
+    return v === true
+  },
+  isFalse: function (v) {
+    return v === false
+  },
+  //未定义
+  isUndef: function (v) {
+    return v === undefined || v === null
+  },
+  //已定义
+  isDef: function (v) {
+    return v !== undefined && v !== null
+  },
+  //isPrimitive
+  isPrimitive: function (v) {
+    typeof v === 'string' ||
+      typeof v === 'number' ||
+      typeof v === 'symbol' ||
+      typeof v === 'boolean'
+  },
+  isObject: function (obj) {
+    return obj !== null && typeof obj === 'object'
+  },
+  // 纯javascript对象
+  isPlainObject: function (obj) {
+    return _toString.call(obj) === '[object Object]'
+  },
+  //是否是RegExp对象
+  isRegExp: function (obj) {
+    return _toString.call(v) === '[object RegExp]'
+  },
+  // Check if val is a valid array index
+  isValidArrayIndex: function (val) {
+    var n = parseFloat(String(val));
+    return n >= 0 && Math.floor(n) === n && isFinite(val)
+  },
+  /**
+   * 转字符串
+   * null 和 undefined 转成空字符转，对象转成格式化的JSON字符串，其他调用String()
+   */
+  toString: function (val) {
+    return val == null
+      ? ''
+      : typeof val === 'object'
+        ? JSON.stringify(val, null, 2)
+        : String(val)
+  },
+  /**
+   * 字符串转化为数字
+   * 转化失败返回原字符串
+   */
+  toNumber: function (val) {
+    var n = parseFloat(val);
+    return isNaN(n) ? val : n
+  },
+  //数组中删除一项
+  remove: function (arr, item) {
+    if (arr.length) {
+      var index = arr.indexOf(item);
+      if (index > -1) {
+        return arr.splice(index, 1)
+      }
+    }
+  },
+  //对象是否有某个属性
+  hasOwn: function (obj, key) {
+    return hasOwnProperty.call(obj, key)
+  },
+  //将类数组的对象转换成数组
+  toArray: function (list, start) {
+    start = start || 0;
+    var i = list.length - start;
+    var ret = new Array(i);
+    while (i--) {
+      ret[i] = list[i + start];
+    }
+    return ret
+  },
+  //混合属性到目标属性
+  extend: function (to, _from) {
+    for (var key in _from) {
+      to[key] = _from[key];
+    }
+    return to
+  },
+  // 合并Array数组中的每一个对象到一个新的Object中
+  toObject: function (arr) {
+    const res = {}
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i]) {
+        Util.extend(res, arr[i])
+      }
+    }
+    return res
+  },
+  //检测两个变量是否相等
+  looseEqual: function (a, b) {
+    if (a === b) { return true }
+    var isObjectA = Util.isObject(a);
+    var isObjectB = Util.isObject(b);
+    if (isObjectA && isObjectB) {
+      try {
+        var isArrayA = Array.isArray(a);
+        var isArrayB = Array.isArray(b);
+        if (isArrayA && isArrayB) {
+          return a.length === b.length && a.every(function (e, i) {
+            return looseEqual(e, b[i])
+          })
+        } else if (!isArrayA && !isArrayB) {
+          var keysA = Object.keys(a);
+          var keysB = Object.keys(b);
+          return keysA.length === keysB.length && keysA.every(function (key) {
+            return looseEqual(a[key], b[key])
+          })
+        } else {
+          /* istanbul ignore next */
+          return false
+        }
+      } catch (e) {
+        /* istanbul ignore next */
+        return false
+      }
+    } else if (!isObjectA && !isObjectB) {
+      return String(a) === String(b)
+    } else {
+      return false
+    }
+  },
+  // 检测arr数组中是否包含与val变量相等的项
+  looseIndexOf: function (arr, val) {
+    for (var i = 0; i < arr.length; i++) {
+      if (Util.looseEqual(arr[i], val)) { return i }
+    }
+    return -1
+  },
+  // 取[begin, end]间的数据整数
+  GetRandom: function (begin, end) {
+    var num = Math.random() * 100000000;
+    return Math.floor(num % (end - begin + 1) + begin);
+  },
+  // 取len长的随机字符串
+  GetRandString: function (len, range) {
+    range = range || "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var s = range.split('');
+    var ret = '';
+    for (var i = 0; i < len; i++) {
+      ret += s[Util.GetRandom(0, s.length - 1)];
+    }
+    return ret;
+  },
+  http: http
+}
+
+export default Util
